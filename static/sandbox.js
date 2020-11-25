8bf83b350a922f85f8e4bfb7a912a9e0f3c6af02
Phong Pham Nguyen Quang <phong.pham@axonactive.com>
2020-11-25 16:12:29 +0700

    Add dummy products

--- static/sandbox.js
+++ static/sandbox.js
@@ -1,3 +1,12 @@
+const dummyProducts = {	
+    airpods: { name: "Airpods", price: "199", img: "airpods.jpg" },	
+    shoe: { name: "Air Jordans", price: "125", img: "air-jordans.jpg" },	
+    iphone: { name: "Smartphone", price: "699", img: "smartphone.jpg" },	
+    book: { name: "Book", price: "15", img: "book.jpg" },	
+    bike: { name: "Bike", price: "800", img: "bike.jpg" },	
+    videoGame: { name: "Gaming Console", price: "299", img: "video-game.jpg" }	
+}
+
 // Init sandbox products
 var pricingTables = document.getElementById("pricing-tables");
 for (const key in dummyProducts) {
